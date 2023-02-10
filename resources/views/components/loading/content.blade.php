4182e421efcf0d8c70fcb0ab9d1995a8f981e49f
İsmet Şafak <safakismet@gmail.com>
2023-02-10 15:35:01 +0300

    add loading animation

--- resources/views/components/loading/content.blade.php
+++ resources/views/components/loading/content.blade.php
@@ -5,6 +5,9 @@
   class="fixed w-full lg:w-4/5 h-screen flex items-center justify-center bg-body top-0 bottom-0 ltr:right-0 rtl:left-0 -mx-1 z-50"
   style="z-index: 60;"
 >
-  <img src="{{ asset('public/img/akaunting-loading.gif') }}" class="w-28 h-28 lg:-mt-16" alt="Workhy Books" />
+<div class="w-4/5">
+  <Lottie />
+</div>
+
 </div>
 <!--data attr added because for none vue.js pages-->
