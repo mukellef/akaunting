ca49af6b9b689efd84139dd82c640e753963e79d
İsmet Şafak <safakismet@gmail.com>
2023-02-15 13:34:26 +0300

    test send commit

--- resources/views/components/loading/content.blade.php
+++ resources/views/components/loading/content.blade.php
@@ -2,7 +2,7 @@
   x-data="{ }"
   x-init="document.querySelector('[data-modal-handle]') ? document.querySelectorAll('[data-modal-handle]').forEach((item) => { item.classList.add('invisible') }) : null, setTimeout(() => $refs.loadingContent.remove(), 1000), setTimeout(() => document.querySelector('[data-modal-handle]') ? document.querySelectorAll('[data-modal-handle]').forEach((item) => { item.classList.remove('invisible') }) : null , 1010)"
   x-ref="loadingContent"
-  class="fixed w-full lg:w-4/5 h-screen flex items-center justify-center bg-body top-0 bottom-0 ltr:right-0 rtl:left-0 -mx-1 z-50"
+  class="fixed w-full lg:w-4/5 h-screen flex items-center bg-body justify-center top-0 bottom-0 ltr:right-0 rtl:left-0 -mx-1 z-50"
   style="z-index: 60;"
 >
 <div class="lg:w-4/5 w-full">
