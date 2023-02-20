98178459343f49a1a6272bc7eefab0400d37ce10
heisenberg <psychaos@gmail.com>
2023-02-21 00:23:56 +0300

    add sso for workhy

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,6 +21,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\FrameGuard::class,
     ];
 
     /**
@@ -151,6 +152,7 @@ class Kernel extends HttpKernel
         // Laravel
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'auth.workhy' => \App\Http\Middleware\WorkhyAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
