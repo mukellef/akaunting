b889f16be03f0c478381b7bee2eab2e1d608c030
sistem-mukellef <sistem@mukellef.co>
2024-11-26 17:44:12 +0300

    Remove docker-compose from deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-docker-compose exec php composer install
-docker-compose exec php npm install
-docker-compose exec php php php artisan migrate --force -n
-docker-compose exec php php artisan module:migrate --force -n
-docker-compose exec php php artisan queue:restart
-docker-compose exec php php artisan config:clear
-docker-compose exec php php artisan cache:clear
-docker-compose exec php php artisan route:clear
+composer install
+npm install
+php artisan migrate --force -n
+php artisan module:migrate --force -n
+php artisan queue:restart
+php artisan config:clear
+php artisan cache:clear
+php artisan route:clear
+
